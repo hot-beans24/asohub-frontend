797626ae79a102f88b327051091c754b937bbb6b
nimame <fabean2224bn@gmail.com>
2023-09-12 10:41:05 +0900

    小文字から大文字へ

--- src/components/ButtonGroup/styles.ts
+++
@@ -1,8 +0,0 @@
-import { css } from '@emotion/react'
-
-export const group = css`
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`
--- src/components/Form/FormFieldLabel/index.tsx
+++
@@ -1,9 +0,0 @@
-import { FC, PropsWithChildren } from 'react'
-
-import { label } from './styles'
-
-const FormFieldLabel: FC<PropsWithChildren> = ({ children }) => {
-  return <span css={label}>{children}</span>
-}
-
-export default FormFieldLabel
