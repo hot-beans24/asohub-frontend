819dad1930e00bacc013d80fc4fc2f329d581d6b
nimame <fabean2224bn@gmail.com>
2023-10-19 10:20:36 +0900

    404NotFoundページファイル作成

---
+++ src/pages/NotFound.page.tsx
@@ -0,0 +1,10 @@
+import { FC } from 'react'
+
+import PageStatus from '@@/features/common/components/PageStatus'
+
+/* ⭐️ 404NotFoundページ : 完 ⭐️ */
+const NotFoundPage: FC = () => {
+  return <PageStatus status="notfound" />
+}
+
+export default NotFoundPage
